d840ccb6a62bf845fe938765f9b1d3bd709176ab
ZhuGuodong <1353819161@qq.com>
2015-10-11 13:56:16 +0800

    Create funny.c

---
+++ funny.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,x;
+
+	printf("亲爱的欧尼酱下午好，欢迎使用VC6.0\n");
+
+	printf("请问需要几杯妹汁和薯条:\n");
+	scanf("%d,%d",&a,&b);
+
+	x=a+b;
+	printf("好的请用%d\n",x);
+
+	return 0;
+}
